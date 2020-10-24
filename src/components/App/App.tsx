7b296ecb6c82ecb87188907498c3e36e99901ed3
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-24 15:52:32 -0600

    Add filler text for styling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,6 +9,7 @@ const App: React.FC = () => {
   return (
 		<main className="App">
 			<Header />
+			<p>This is some text so we can see what these cool fonts look like and if we like them we are going to KEEP USING THEM!</p>
       <Switch>
         {Object.values(routes).map((route:any, index:number) => (
           <Route key={index} {...route} path={route.path} />
