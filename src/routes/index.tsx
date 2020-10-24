6e06a076425ab64d35617b71d515d4c61f8fefcc
Amy Karnaze <akarnaze@gmail.com>
2020-10-24 12:13:42 -0600

    compile routes #14

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import applicant from './applicant'
+import searchResults from './searchResults'
+
+// assigne cp
+export const routes = Object.assign(applicant, searchResults)
+
+// or
+
+// export const routes = [applicant, searchResults]
