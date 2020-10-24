704ed8b922e70ead361f1f15859908895cc96cdc
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-24 17:16:43 -0600

    Update style on main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,16 +7,15 @@ import Header from '../../components/Header/Header'
 
 const App: React.FC = () => {
   return (
-		<main className="App">
+		<div className="App">
 			<Header />
-			<p>This is some text so we can see what these cool fonts look like and if we like them we are going to KEEP USING THEM!</p>
       <Switch>
         {Object.values(routes).map((route:any, index:number) => (
           <Route key={index} {...route} path={route.path} />
         ))
         }
       </Switch>
-		</main>
+		</div>
   )
 }
 
