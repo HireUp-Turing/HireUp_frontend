10ba5ffb837ecdc8f74ac24b52a05597ea5ff793
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-24 19:35:39 -0600

    Add alt text to img

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
 	return (
 		<main>
 			<section className="about">
-				<img src='../images/clipboard.png' />
+				<img src='../images/clipboard.png' alt="clipboard"/>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus viverra adipiscing at in tellus integer. Tristique sollicitudin nibh sit amet. Habitant morbi tristique senectus et netus et malesuada fames ac. Ornare aenean euismod elementum nisi. Rhoncus aenean vel elit scelerisque mauris.</p>
 			</section>
 			<nav className="cta-buttons">
