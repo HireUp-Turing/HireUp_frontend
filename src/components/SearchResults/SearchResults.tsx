1968b53d800d5735d8f5600040ef6f7fa7e831a3
Amy Karnaze <akarnaze@gmail.com>
2020-10-26 19:18:00 -0600

    Map fake user data to Applicant Preview #26

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from 'react'
+
+import { users } from '../../assets/fake-users'
+import ApplicantPreview from '../ApplicantPreview/ApplicantPreview'
+import './SearchResults.scss'
 
 const SearchResults: React.FC = () => {
-return <div>Hello World</div>
+  let userData = users.map((user) => {
+    return ( 
+      <ApplicantPreview 
+        id={user.id}
+        username={user.username}
+        bio={user.bio}
+        updated_at={user.updated_at}
+        skills={user.skills}
+        values={user.values}
+      />
+    )
+  })
+  return (
+    <main className="search-results">
+      {userData}
+      <p>Hello Search Results</p>
+    </main>
+  )
 }
 
 export default SearchResults
