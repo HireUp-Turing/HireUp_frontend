5205d2bc886a40d862860e5477f645b521f0d2bf
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-28 18:44:56 -0600

    Add style to Applicant #34

--- src/components/Applicant/Applicant.tsx
+++ src/components/Applicant/Applicant.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { ApplicantProfile } from '../../assets/definitions'
 import { getApplicantById } from '../../assets/api-calls'
 import { RouteComponentProps } from 'react-router-dom'
+import './Applicant.scss'
 
 const Applicant: React.FC<RouteComponentProps> = (props) => {
   const [applicant, setApplicant] = useState<ApplicantProfile>({username: '', bio: '', skills: [], values: []})
@@ -30,7 +31,7 @@ const Applicant: React.FC<RouteComponentProps> = (props) => {
 
 const determineMatchedAttribute = (attribute: string, keyword: string, props: any):string => {
 		if (props && props.attributeMatches[keyword][0] && props.attributeMatches[keyword][0].attribute === attribute) {
-      return 'attribute-tag-highlight'
+      return 'attribute-tag highlight'
     } else {
       return 'attribute-tag'
 		}
@@ -57,20 +58,30 @@ const determineMatchedAttribute = (attribute: string, keyword: string, props: an
   }
 
   return (
-    <main className="">
-      <img 
-        className="applicant-icon"
-        src={`https://avatars.dicebear.com/api/bottts/${applicant.username}.svg`} 
-        alt={`${applicant.username}'s icon`}
-      />
-      <h2>{applicant.username}</h2> 
-      <p>{applicant.bio}</p>  
-      <div>
-        <h4>Skills</h4>
-        {attributeLists.skillTags}
-        <h4>Values</h4>
-        {attributeLists.valueTags}
-      </div>
+    <main className="Applicant">
+			<span className="applicant-border">
+				<div className="applicant-profile">
+					<img 
+						className="applicant-icon"
+						src={`https://avatars.dicebear.com/api/bottts/${applicant.username}.svg`} 
+						alt={`${applicant.username}'s icon`}
+					/>
+					<div className="applicant-info">
+						<h2 className="username">{applicant.username}</h2> 
+						<p>{applicant.bio}</p>  
+					</div>
+				</div>
+				<div className="applicant-attributes">
+					<div className="attribute-box">
+						<h5>Skills</h5>
+						{attributeLists.skillTags}
+					</div>
+					<div className="attribute-box">
+						<h5>Values</h5>
+						{attributeLists.valueTags}
+					</div>
+				</div>
+			</span>
     </main>
   )
 }
