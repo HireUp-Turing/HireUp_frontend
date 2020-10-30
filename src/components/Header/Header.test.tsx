c608362a292e69897f2d0e33292d248925d1f838
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-10-30 12:55:49 -0600

    Write test for Home render #50

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
 import Header from './Header'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('Header Component', () => {
