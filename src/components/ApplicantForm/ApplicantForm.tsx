b1c45dee52b47ad7d752b4eab90aef1928533441
Amy Karnaze <akarnaze@gmail.com>
2020-10-29 19:27:08 -0600

    Add structure to form and map skills for styling

--- src/components/ApplicantForm/ApplicantForm.tsx
+++ src/components/ApplicantForm/ApplicantForm.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 import { getNames } from '../../assets/api-calls'
 import './ApplicantForm.scss'
+import { skillsData, valuesData } from '../../assets/test-values-skills'
 
 const ApplicantForm: React.FC = () => {
 	const [username, setUsername] = useState<string>('')
@@ -15,47 +16,55 @@ const ApplicantForm: React.FC = () => {
 		setUsername(name)
 	}
 
+	const skills = () => {
+		return skillsData.map((skill: {attribute:string}) => {
+			return <button className="attribute-tag">{skill.attribute}</button>
+		})
+	}
+
 	return (
-		<main className="Applicant">
-			<span className="applicant-border">
+		<main>
+			<div className="new-applicant-container">
 				<div className="applicant-profile">
 					<img 
 						className="applicant-icon"
 						src={`https://avatars.dicebear.com/api/bottts/${username}.svg`} 
 						alt="default user icon"
 					/>
-					<div className="applicant-info">
+					<div>
 						<h5>Enter your information below to create a profile</h5>
-						<label htmlFor="first-name">First Name</label>
-						<input id="first-name" className="applicant-input" />
-						<label htmlFor="last-name">Last Name</label>
-						<input id="last-name" className="applicant-input" />
-						<label htmlFor="email">Email</label>
-						<input id="email" className="applicant-input" />
-						<label htmlFor="bio">Bio</label>
-						<input id="bio" className="applicant-input" />
-						<label htmlFor="codename">Codename
+						<h6><label htmlFor="codename">Codename
 						<img className="refresh-button" src="./refresh-pink.svg" alt="refresh-button"
 							onClick={ async () => {
 								makeUserName()
 							}}
 							/>
-						</label>
+						</label></h6>
 						<input id="codename" className="applicant-input" placeholder={username} disabled/>
+						<label htmlFor="codename">Refresh to find your perfect anonymized codename</label>
+						<h6><label htmlFor="bio">Bio</label></h6>
+						<input id="bio" className="applicant-input" />
+						<p>The following information will not be shared with employers:</p>
+						<h6><label htmlFor="first-name">First Name</label></h6>
+						<input id="first-name" className="applicant-input" />
+						<h6><label htmlFor="last-name">Last Name</label></h6>
+						<input id="last-name" className="applicant-input" />
+						<h6><label htmlFor="email">Email</label></h6>
+						<input id="email" className="applicant-input" />
 					</div>
 				</div>
-				<h5>Select your skills and values</h5>
 				<div className="applicant-attributes">
-					<div className="attribute-box">
+				<h5>Select your skills and values</h5>
 						<h5>Skills</h5>
-						skills were here
-					</div>
 					<div className="attribute-box">
+						{skills()}
+					</div>
 						<h5>Values</h5>
+					<div className="attribute-box">
 						values were here
 					</div>
 				</div>
-			</span>
+			</div>
     </main>
 	)
 }
