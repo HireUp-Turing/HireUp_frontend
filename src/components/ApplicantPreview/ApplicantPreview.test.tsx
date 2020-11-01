856a7056b240ac359b4349eb9cc74329ec9b9635
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-11-01 16:46:19 -0700

    Write test that avatar is rendered #50

--- src/components/ApplicantPreview/ApplicantPreview.test.tsx
+++ src/components/ApplicantPreview/ApplicantPreview.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ApplicantPreview from './ApplicantPreview'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('ApplicantPreview Component', () => {
 
-	it('Should display a codename and bio', async () => {
-		const { findByRole, findByText } = render(
+	it('Should display a codename and bio', () => {
+		render(
 			<MemoryRouter>
 				<ApplicantPreview
 					id={1}
@@ -21,9 +21,30 @@ describe('ApplicantPreview Component', () => {
 				/>
 			</MemoryRouter>
 		)
-		const username = await findByRole('heading', { name: /busy bee/i })
-		const bio = await findByText(/i like to code!/i)
+		const username = screen.getByRole('heading', { name: /busy bee/i })
+		const bio = screen.getByText(/i like to code!/i)
 		expect(username).toBeInTheDocument()
 		expect(bio).toBeInTheDocument()
 	})
+
+	it('Should display an avatar', () => {
+		render(
+			<MemoryRouter>
+				<ApplicantPreview
+					id={1}
+					username="busy bee"
+					bio="I like to code!"
+					skills={[{attribute: "ruby", id: 1}]}
+					values={[{attribute: "teamwork", id: 1}]}
+					query={{query: {
+						skills: [{attribute: "ruby", checked: true}],
+						values: [{attribute: "teamwork", checked: true}]
+					}}}
+				/>
+			</MemoryRouter>
+		)
+		const avatar = screen.getByAltText(/busy bee\'s icon/i)
+		expect(avatar).toBeInTheDocument()
+	})
+
 })
