b1af90a57cb358242a537d49b77fb9431d497615
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-11-01 13:53:14 -0700

    Write test that search options are fetched upon load #50

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,11 +1,52 @@
 import React from 'react'
 import Search from './Search'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
 import { MemoryRouter } from 'react-router-dom'
+import { getSearchOptions } from '../../assets/api-calls'
+jest.mock('../../assets/api-calls')
 
 describe('Search Component', () => {
+	let mockedSearchOptions: { success: boolean, data: Array<any> }
+
+	beforeEach(() => {
+		mockedSearchOptions = {
+			"success": true,
+			"data": [
+				{
+					"skills": [
+						{
+							"id": 2,
+							"attribute": "flask"
+						},
+						{
+							"id": 1,
+							"attribute": "rails"
+						},
+						{
+							"id": 3,
+							"attribute": "ruby"
+						}
+					],
+					"values": [
+						{
+							"id": 1,
+							"attribute": "creativity"
+						},
+						{
+							"id": 2,
+							"attribute": "mentorship"
+						}
+					]
+				}
+			]
+		}
+	})
 
 	it('Should display a title and two prompts', () => {
+		mocked(getSearchOptions).mockImplementation(() =>
+			Promise.resolve(mockedSearchOptions)
+		)
 		render(<MemoryRouter><Search /></MemoryRouter>)
 		const title = screen.getByRole('heading', { name: /find applicants/i })
 		const prompt1 = screen.getByText(/skills/i)
@@ -16,6 +57,9 @@ describe('Search Component', () => {
 	})
 
 	it('Should display two buttons', () => {
+		mocked(getSearchOptions).mockImplementation(() =>
+			Promise.resolve(mockedSearchOptions)
+		)
 		render(<MemoryRouter><Search /></MemoryRouter>)
 		const searchButton = screen.getByRole('link', { name: /search/i })
 		const clearButton = screen.getByRole('button', { name: /clear/i })
@@ -23,9 +67,18 @@ describe('Search Component', () => {
 		expect(clearButton).toBeInTheDocument()
 	})
 
-	// it('Should fetch search options upon load', () => {
-	// 	render(<MemoryRouter><Search /></MemoryRouter>)
-	// })
+	it('Should fetch search options upon load', async () => {
+		mocked(getSearchOptions).mockImplementation(() =>
+			Promise.resolve(mockedSearchOptions)
+		)
+		const { findByText } = render(<MemoryRouter><Search /></MemoryRouter>)
+		const skill1 = await findByText(/flask/i)
+		const skill2 = await findByText(/rails/i)
+		const skill3 = await findByText(/ruby/i)
+		expect(skill1).toBeInTheDocument()
+		expect(skill2).toBeInTheDocument()
+		expect(skill3).toBeInTheDocument()
+	})
 
 	// it('Should fire the correct method when search button clicked', () => {
 	// 	render(<MemoryRouter><Search /></MemoryRouter>)
