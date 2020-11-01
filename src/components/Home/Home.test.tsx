18b278a457d664b2134912e19d1c362cfb7fefaf
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-11-01 11:51:53 -0700

    Write test for find applicants button #50

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Home from './Home'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import { OpenMenuContext } from '../../contexts/index'
 
 describe('Home Component', () => {
 
@@ -19,6 +20,25 @@ describe('Home Component', () => {
 		const applicantButton = screen.getByRole('link', { name: /i\'m an applicant/i })
 		expect(findApplicantsButton).toBeInTheDocument()
 		expect(applicantButton).toBeInTheDocument()
+	})
+
+	it('Should fire the correct method when Find Applicants is clicked', () => {
+		const isOpen = false
+		const setIsHidden = jest.fn()
+		const toggleMenu = jest.fn()
+		render(
+			<MemoryRouter>
+				<OpenMenuContext.Provider value={{ isOpen, setIsHidden, toggleMenu }}>
+					<Home />
+				</OpenMenuContext.Provider>
+			</MemoryRouter>
+		)
+		const findApplicantsButton = screen.getByRole('button', { name: /find applicants/i })
+		fireEvent.click(findApplicantsButton)
+		expect(setIsHidden).toBeCalledTimes(1)
+		expect(toggleMenu).toBeCalledTimes(1)
+		fireEvent.click(findApplicantsButton)
+		expect(toggleMenu).toBeCalledTimes(2)
 	})
 
 })
