c3fa6d2c497c3f39ade37c98f031e6c207bd4ee7
Amy Karnaze <akarnaze@gmail.com>
2020-11-01 23:43:47 -0700

    Add 2 new tests

--- src/components/MessageForm/MessageForm.test.tsx
+++ src/components/MessageForm/MessageForm.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen, fireEvent, findByText, waitFor} from "@testing-library/react";
 import MessageForm from "./MessageForm";
 // import { mocked } from 'ts-jest/utils';
+import { shallow } from 'enzyme';
 import '@testing-library/jest-dom'
 import { MemoryRouter } from "react-router-dom";
 import { AuthContext } from '../../contexts/index'
@@ -31,5 +32,72 @@ describe('MessageForm', () => {
     expect(messageInput).toBeInTheDocument()
     expect(emailInput).toBeInTheDocument()
     expect(nameInput).toBeInTheDocument()
+  })
+
+  it('Send should fire an event', async () => {
+    const routeComponentPropsMock = {
+      history: {} as any,
+      location: {} as any,
+      match: {
+        isExact: true,
+        params: {id: 1},
+        path: "",
+        url: ""
+      }
+    }
+    const auth = 1
+    render(<MemoryRouter><AuthContext.Provider value={{auth}}><MessageForm {...routeComponentPropsMock} /></AuthContext.Provider></MemoryRouter>)
+
+    const sendButton = screen.getByPlaceholderText("send", {exact:false})
+    // const sendButton = screen.getByRole("button", {name:"send", exact: false})
+    // const sendButton = await screen.getByRole("button", {name:"send", exact: false})
+    expect(sendButton).toBeInTheDocument()
+    // const mockFn= jest.fn()
+    sendButton.simulate('click')
+    fireEvent.click(sendButton)
+    // expect(mockFn)toHaveBeenCalled()
+    await expect(sendButton).toBeCalledTimes(1)
+  })
+
+  it('input should reflect a change', async () => {
+    const routeComponentPropsMock = {
+      history: {} as any,
+      location: {} as any,
+      match: {
+        isExact: true,
+        params: {id: 1},
+        path: "",
+        url: ""
+      }
+    }
+    const auth = 1
+    render(<MemoryRouter><AuthContext.Provider value={{auth}}><MessageForm {...routeComponentPropsMock} /></AuthContext.Provider></MemoryRouter>)
+
+    const sendButton = screen.getByPlaceholderText("Send", {exact:false})
+    const nameInput = screen.getByLabelText("Your name", {exact: false})
+    const emailInput = screen.getByLabelText("The best email to reach you", {exact: false})
+    const messageInput = screen.getByLabelText("Your message here", {exact: false})
+    
+    // expect(messageInput).toBeInTheDocument()
+    // expect(emailInput).toBeInTheDocument()
+    // expect(nameInput).toBeInTheDocument()
+  
+    expect(sendButton).toBeInTheDocument()
+    expect(nameInput.value).toBe('')
+    expect(emailInput.value).toBe('')
+    expect(messageInput.value).toBe('')
+
+    fireEvent.change(emailInput, {target: {name: 'email', value: 'New-Title'}})
+    await expect(emailInput.value).toEqual('New-Title')
+
+    fireEvent.change(nameInput, {target: {name: 'name', value: 'New-Title'}})
+    await expect(emailInput.value).toEqual('New-Title')
+
+    fireEvent.change(messageInput, {target: {name: 'message', value: 'New-Title'}})
+    await expect(emailInput.value).toEqual('New-Title')
+
+  
+  
+
   })
 })
