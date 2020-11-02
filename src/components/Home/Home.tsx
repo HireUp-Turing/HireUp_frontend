bc9041893e5a99dc24abbf89704519bde293f9f4
Greyson Elkins <greysonelkins@gmail.com>
2020-11-02 16:45:29 -0700

    Add new home page blurb #60

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,12 @@ const Home: React.FC = () => {
 		<main className="Home">
 			<section className="about">
 				<img src='../images/clipboard.png' alt="clipboard"/>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus viverra adipiscing at in tellus integer. Tristique sollicitudin nibh sit amet. Habitant morbi tristique senectus et netus et malesuada fames ac. Ornare aenean euismod elementum nisi. Rhoncus aenean vel elit scelerisque mauris.</p>
+				<p>HireUp aims to minimize bias in the hiring process by turning applications into true Classifieds! 
+				Our anonomyzed job applicants are the best in the buis. By eliminating paperwork (CVs, resumes, etc) we've boiled down
+				the application process to the things you care about - skills and values. <br />
+
+				If you're looking for new staff go ahead and start searching our profiles! You'll be able to directly message applicants through our site.
+				If you're an applicant, start out by submitting a profile with us below! </p>
 			</section>
 			<nav className="cta-buttons">
 				<OpenMenuContext.Consumer>
