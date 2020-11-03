8747454292610af5f13adc71b87213022171e079
Amy Karnaze <akarnaze@gmail.com>
2020-11-03 16:20:21 -0700

    Add skipped test to check a form is rendered

--- src/components/ApplicantForm/ApplicantForm.test.tsx
+++ src/components/ApplicantForm/ApplicantForm.test.tsx
@@ -10,11 +10,31 @@ import { getAttributes } from '../../assets/api-calls'
 jest.mock('../../assets/api-calls')
 
 describe('ApplicantForm', () => {
-  it('should render a form', async () => {
+  it.skip('should render a form', async () => {
 
-    // mockedAttributes = Need mock here!
+   const mockedAttributes = {
+     "success": true,
+     "data": [
+       {
+        "id": 1,
+        "attribute": "rails"
+       },
+       {
+        "id": 3,
+        "attribute": "ruby"
+      },
+      {
+        "id": 29,
+        "attribute": "leadership"
+      },
+      {
+        "id": 30,
+        "attribute": "organization"
+      }
+     ]
+    }
 
-    const routeComponentPropsMock =
+    const routeComponentPropsMock = {
       history: {} as any,
       location: {} as any,
       match: {
@@ -25,32 +45,31 @@ describe('ApplicantForm', () => {
       }
     }
 
-    // mocked(getAttributes).mockImplementation(() => 
-    //   Promise.resolve(mockedAttributes))
+    mocked(getAttributes).mockImplementation(() => 
+    Promise.resolve(mockedAttributes))
 
     const auth = 1
+    console.error = jest.fn()
     render(<MemoryRouter><AuthContext.Provider value={{auth}}><ApplicantForm {...routeComponentPropsMock} /></AuthContext.Provider></MemoryRouter>)
 
-    const lastNameInput = screen.getByLabelText("First Name", {exact: false})
-    const firstNameInput = screen.getByLabelText("Last Name", {exact: false})
-    const emailInput = screen.getByLabelText("The best email to reach you", {exact: false})
-    const BioInput = screen.getByLabelText("Your message here", {exact: false})
-    const createButton = screen.getByRole("button", { name: "create your profile", exact:false})
-    const skill1 = await screen.findByText(/flask/i)
-		const skill2 = await screen.findByText(/rails/i)
-		const skill3 = await screen.findByText(/ruby/i)
-		const value1 = await screen.findByText(/creativity/i)
-    const value2 = await screen.findByText(/mentorship/i)
-    
-    expect(lastNameInput).toBeInTheDocument()
-    expect(firstNameInput).toBeInTheDocument()
-    expect(emailInput).toBeInTheDocument()
-    expect(BioInput).toBeInTheDocument()
-    expect(createButton).toBeInTheDocument()
-		expect(skill1).toBeInTheDocument()
-		expect(skill2).toBeInTheDocument()
-		expect(skill3).toBeInTheDocument()
-		expect(value1).toBeInTheDocument()
-		expect(value2).toBeInTheDocument()
+      const lastNameInput = screen.getByLabelText("First Name", {exact: false})
+      const firstNameInput = screen.getByLabelText("Last Name", {exact: false})
+      const emailInput = screen.getByLabelText("email", {exact: false})
+      const BioInput = screen.getByLabelText("bio", {exact: false})
+      const createButton = screen.getByRole("button", { name: "create your profile", exact:false})
+      const skill2 = await screen.findAllByText(/rails/i)
+      const skill1 = await screen.findAllByText(/ruby/i)
+      const value1 = await screen.findAllByText(/leadership/i)
+      const value2 = await screen.findAllByText(/organization/i)
+      
+      expect(lastNameInput).toBeInTheDocument()
+      expect(firstNameInput).toBeInTheDocument()
+      expect(emailInput).toBeInTheDocument()
+      expect(BioInput).toBeInTheDocument()
+      expect(createButton).toBeInTheDocument()
+      expect(skill2.length).toBe(2)
+      expect(skill1.length).toBe(2)
+      expect(value1.length).toBe(2)
+      expect(value2.length).toBe(2)
     })
   })
